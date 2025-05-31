721a5c5209bb8f23bbc2b2641493d8fee9fc37da
Noirxa <1092298@hr.nl>
2025-06-01 00:44:17 +0200

    Remove commented-out code and clean up imports across multiple files for improved readability and maintainability

--- src/js/resources.js
+++ src/js/resources.js
@@ -1,26 +1,4 @@
 
-
-// import { ImageSource, Loader } from "excalibur";
-
-// export const Resources = {
-//   Shooter: new ImageSource("images/shooter.gif"),
-//   Zombie: new ImageSource("images/zombie.gif"),
-//   Bullet: new ImageSource("images/bullet.png"),
-//   Background: new ImageSource("images/grassbackground.jpg"),
-//   FastZombie: new ImageSource("images/fastzombie.png"),
-//   AmmoPickup: new ImageSource("images/ammo.png"), // Nieuw!
-// };
-
-// export const ResourceLoader = new Loader([
-//   Resources.Shooter,
-//   Resources.Zombie,
-//   Resources.Bullet,
-//   Resources.Background,
-//   Resources.FastZombie,
-//   Resources.AmmoPickup, // Nieuw!
-// ]);
-
-
 import { ImageSource, Loader } from "excalibur";
 
 // Alle afbeeldingsbronnen worden hier gedefinieerd en ingeladen
